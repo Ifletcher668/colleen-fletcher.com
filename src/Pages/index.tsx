c24eec0de03620461e83459edf4e75ce08043c33
Isiah <Isiahfletcher7@gmail.com>
2020-11-28 18:19:13 -0700

    adding eslint rules

---
+++ src/Pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import config from '../../config/website'
+
+const Index = () => {
+    console.log('testing eslint')
+
+    return <div>Hello!</div>
+}
+
+export default Index
