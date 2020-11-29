9c9a49fb8d38947ebd6f5fe3d576106150364d66
Isiah <Isiahfletcher7@gmail.com>
2020-11-28 18:30:02 -0700

    updating eslint

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import config from '../../config/website'
+import React from 'react';
+import config from '../../config/website';
 
 const Index = () => {
-    console.log('testing eslint')
+    console.log('testing eslint');
 
-    return <div>Hello!</div>
-}
+    return <div>Hello!</div>;
+};
 
-export default Index
+export default Index;
