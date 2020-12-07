23b12cdaef073eca3a2560a1e15e464da781e6a3
Isiah <Isiahfletcher7@gmail.com>
2020-12-06 18:19:04 -0700

    updating packages

--- config/website.ts
+++ config/website.ts
@@ -1,3 +1,53 @@
-const config = {}
+const config: Config = {
+    routes: {
+        home: {
+            name: 'Home',
+            path: '/',
+        },
+        about: {
+            name: 'About Me',
+            path: '/about-me',
+        },
+        blogs: {
+            name: 'Blogs',
+            path: '/blogs',
+            submenu: [
+                {
+                    name: 'Tuesday Tips',
+                    path: '/tuesday-tips',
+                },
+                {
+                    name: 'You, Me, and the Trees',
+                    path: '/you-me-and-the-trees',
+                },
+            ],
+        },
+        workWithMe: {
+            name: 'Work With Me',
+            path: '/work-with-me',
+            submenu: [
+                {
+                    name: 'Reiki',
+                    path: '/tuesday-tips',
+                },
+                {
+                    name: 'Classes and Workshops',
+                    path: '/you-me-and-the-trees',
+                },
+            ],
+        },
+        products: {
+            name: 'Products',
+            path:
+                'https://wholisticbeautyboutique.com/collections/sale-products',
+            external: true,
+        },
+    },
+    socials: {
+        instagram: 'instagram',
+        facebook: 'facebook',
+        discord: 'discord',
+    },
+};
 
-export default config
+export default config;
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,51 @@
+const config = require('./config/metaData.js');
+const path = require('path');
 
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
- 
-  plugins: [],
-}
+    siteMetadata: {
+        siteUrl: config.siteURl,
+        title: config.siteTitle,
+        description: config.siteDescription,
+    },
+    plugins: [
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-sass`,
+        // {
+        //     resolve: `gatsby-plugin-manifest`,
+        //     options: {
+        //         name: 'isiahfletcher.com',
+        //         short_name: 'Isiah Fletcher',
+        //         start_url: '/',
+        //         icon: './static/images/isiah-fletcher-logo.png',
+        //     },
+        // },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/assets/images`,
+            },
+        },
+        // TODO: uncomment when building Strapi
+        // {
+        //     resolve: 'gatsby-source-graphql',
+        //     options: {
+        //         typeName: 'STRAPI',
+        //         fieldName: 'strapi',
+        //         url: `${
+        //             process.env.NODE_ENV === 'development'
+        //                 ? 'http://localhost:1337/graphql'
+        //                 : 'https://isiahfletcheradmin.herokuapp.com/graphql'
+        //         }`,
+        //         refetchInterval: process.env.NODE_ENV === 'development' && 50, // refetches Strapi data every 10 minutes
+        //     },
+        // },
+    ],
+};
