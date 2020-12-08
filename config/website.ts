c50a8e12725cedb18984cf34c9efd37b2a5ba9bd
Isiah <Isiahfletcher7@gmail.com>
2020-12-08 13:09:40 -0700

    refactoring data layout

--- config/website.ts
+++ config/website.ts
@@ -1,46 +1,102 @@
 const config: Config = {
-    routes: {
+    pages: {
         home: {
             name: 'Home',
-            path: '/',
+            route: {
+                url: '/',
+                isExternalLink: false,
+                data: null,
+            },
+            meta: {
+                seo: {
+                    description: 'Home Page',
+                },
+            },
         },
         about: {
             name: 'About Me',
-            path: '/about-me',
+            route: {
+                url: '/about-me',
+                isExternalLink: false,
+                data: null,
+            },
+            meta: {
+                seo: {
+                    description: 'About Page',
+                },
+            },
         },
         blogs: {
             name: 'Blogs',
-            path: '/blogs',
-            submenu: [
-                {
-                    name: 'Tuesday Tips',
-                    path: '/tuesday-tips',
+            route: {
+                url: '/blogs',
+                isExternalLink: false,
+                // TODO: Still hardcoded
+                data: [
+                    {
+                        name: 'Tuesday Tips',
+                        path: '/tuesday-tips',
+                        data: {
+                            sentence: 'testing submenu length is too long?',
+                        },
+                    },
+                    {
+                        name: 'You, Me, and the Trees',
+                        path: '/you-me-and-the-trees',
+                        data: {
+                            sentence: 'testing submenu length is too long?',
+                        },
+                    },
+                ],
+            },
+            meta: {
+                seo: {
+                    description: 'Blogs Page',
                 },
-                {
-                    name: 'You, Me, and the Trees',
-                    path: '/you-me-and-the-trees',
-                },
-            ],
+            },
         },
         workWithMe: {
             name: 'Work With Me',
-            path: '/work-with-me',
-            submenu: [
-                {
-                    name: 'Reiki',
-                    path: '/tuesday-tips',
+            route: {
+                url: '/work-with-me',
+                isExternalLink: false,
+                // TODO: Still hardcoded
+                data: [
+                    {
+                        name: 'Reiki',
+                        path: '/tuesday-tips',
+                        data: {
+                            sentence: 'testing submenu length is too long?',
+                        },
+                    },
+                    {
+                        name: 'Classes and Workshops',
+                        path: '/you-me-and-the-trees',
+                        data: {
+                            sentence: 'testing submenu length is too long?',
+                        },
+                    },
+                ],
+            },
+            meta: {
+                seo: {
+                    description: 'Blogs Page',
                 },
-                {
-                    name: 'Classes and Workshops',
-                    path: '/you-me-and-the-trees',
-                },
-            ],
+            },
         },
         products: {
             name: 'Products',
-            path:
-                'https://wholisticbeautyboutique.com/collections/sale-products',
-            external: true,
+            route: {
+                url:
+                    'https://wholisticbeautyboutique.com/collections/sale-products',
+                isExternalLink: true,
+                data: null,
+            },
+            meta: {
+                seo: {
+                    description: 'Products',
+                },
+            },
         },
     },
     socials: {
