02f6e4ffae0b790944fbb81a44971eae6dea12b4
Isiah <Isiahfletcher7@gmail.com>
2021-01-09 12:11:52 -0700

    refactor and adding conventions

--- src/utils/Breakpoints/breakpoint-handler.ts
+++ src/utils/Breakpoints/breakpoint-handler.ts
@@ -1,37 +1,39 @@
 export default class BreakpointHandler {
-    private getCSSVariable(varName: string) {
+    private _getCSSVariable(varName: string) {
         return getComputedStyle(document.documentElement).getPropertyValue(
             varName,
         );
     }
-    private getFontSize(htmlElement: string) {
+    private _getFontSize(htmlElement: string) {
         return parseFloat(
             getComputedStyle(document.querySelector(htmlElement) as HTMLElement)
                 .fontSize,
         );
     }
 
-    public getBreakpoints = (breakpoints: string[]) => {
+    public getBreakpoints: (breakpoints: string[]) => number[] = (
+        breakpoints: string[],
+    ) => {
         return breakpoints.map(name => {
-            const value = this.getCSSVariable(name).trim();
+            const value = this._getCSSVariable(name).trim();
             let numberValue = '';
             for (let i = 0; i < value.length; i++) {
                 const unit = value[i] + value[i + 1];
                 if (
                     unit === 'em' ||
-                    unit === 'rem' ||
                     unit === 'px' ||
-                    value[i] === '%'
+                    value[i] === '%' ||
+                    unit + value[i + 2] === 'rem'
                 ) {
                     break;
                 }
                 numberValue += value[i];
             }
-            return parseInt(numberValue) * this.getFontSize('body');
+            return parseInt(numberValue) * this._getFontSize('body');
         });
     };
 
-    public getScreenWidth = () => {
+    public getScreenWidth: () => number = () => {
         return Math.max(
             document.body.scrollWidth,
             document.documentElement.scrollWidth,
