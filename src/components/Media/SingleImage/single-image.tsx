e3eb61aaf4c788a4fc3b7105437bac067eef55c5
Isiah <Isiahfletcher7@gmail.com>
2021-02-06 14:20:36 -0700

    caught chance where data is null

--- src/components/Media/SingleImage/single-image.tsx
+++ src/components/Media/SingleImage/single-image.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Image, ImageWithCaption} from '../../Images';
+import { Image, ImageWithCaption } from '../../Images';
 
 export interface Props {
     data: StrapiUploadFile;
 }
 
-const SingleImageField: React.FC<Props> = ({data}: Props) => {
-    return data.caption ? (
+const SingleImageField: React.FC<Props> = ({ data }: Props) => {
+    return data && data.caption ? (
         <ImageWithCaption uploadFile={data} />
     ) : (
         <Image uploadFile={data} />
