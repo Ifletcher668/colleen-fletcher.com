0111d35a995a2de42eb9e6d4aad07046c92ea23c
Isiah <Isiahfletcher7@gmail.com>
2021-02-14 20:38:22 -0700

    Fixing component flow on small screen sizes

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import BannerBackground from 'gatsby-background-image';
 import StrapiDynamicZone from '../StrapiDynamicZone';
 import styled from 'styled-components';
+import BreakpointHandler from '../../utils/Breakpoints/breakpoint-handler';
 
+const { below } = new BreakpointHandler();
 interface Props {
     image: StrapiUploadFile | null;
     banner: StrapiDynamicZone[];
@@ -27,6 +29,10 @@ const BannerBackgroundImage: React.FC<Props> = ({
 const BannerWrapper = styled(BannerBackground)`
     display: flex;
     padding: ${props => props.theme.size.padding.xsmall};
+    ${props =>
+        below(props.theme.breakpoint.small, {
+            flexFlow: 'colum nowrap',
+        })}
 `;
 
 export default BannerBackgroundImage;
