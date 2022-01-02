f54cb48bf7c4ea329ffc0d571bc0aff2205b9a20
Isiah Fletcher <Isiahfletcher7@mail.com>
2022-01-02 11:10:03 -0700

    Fix divider styles

--- src/components/Divider/styles.tsx
+++ src/components/Divider/styles.tsx
@@ -10,28 +10,22 @@ interface Props {
 export const StandardDivider = styled.hr<Props>`
   width: 100%;
   border: 0;
-  border-bottom: solid 1px
+  border-top: solid 1px
     ${props => (props.lineColor ? color(props.lineColor) : color('coffee'))};
   margin: 0 0 ${size('margin', 'large')} 0;
-  height: 30px;
-  border-style: solid;
-  border-color: ${props =>
-    props.lineColor ? color(props.lineColor) : color('coffee')};
-  border-width: 1px 0 0 0;
 
   &:before {
     display: block;
     content: '';
-    height: 28px;
-    margin-top: -30px;
-    border-style: solid;
-    border-color: ${props =>
-      props.lineColor ? color(props.lineColor) : color('coffee')};
-    border-width: 0 0 1px 0;
+    border-bottom: solid 1px
+      ${props => (props.lineColor ? color(props.lineColor) : color('coffee'))};
+    height: 24px;
+    margin-top: -20px;
   }
 `;
 
 export const FancyDivider = styled(Flexbox)`
+  margin: 0 0 ${size('margin', 'large')} 0;
   gap: 2em;
   background: linear-gradient(
     90deg,
