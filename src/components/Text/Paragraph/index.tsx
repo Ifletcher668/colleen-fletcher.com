6d6c7b2af45194c8966ac212483cb292ad0a1912
Isiah Fletcher <Isiahfletcher7@mail.com>
2022-01-02 11:10:25 -0700

    Refactor

--- src/components/Text/Paragraph/index.tsx
+++ src/components/Text/Paragraph/index.tsx
@@ -14,193 +14,169 @@ export interface Props {
   className?: Array<string> | string;
 }
 
+const handleJustifyCases = (alignValue: string): JustifyValues => {
+  switch (alignValue) {
+    case 'ql-align-center':
+      return 'center';
+    case 'ql-align-right':
+      return 'right';
+    default:
+      // ql-align-left, ql-align-justify
+      return 'left';
+  }
+};
+
 const Paragraph = ({ data, className }: Props): JSX.Element | null => {
   if (data === null) return null;
 
-  const { body, alignParagraph, justifyParagraph } = data;
-  // TODO: Add images to parser
+  const { body, alignParagraph } = data;
   const options: HTMLReactParserOptions = {
     replace: domNode => {
       if (!(domNode instanceof Element)) return null;
       const { name, children, attribs } = domNode;
+      switch (name) {
+        case 'a':
+          return (
+            <Strong>
+              <Link to={attribs.href}>{domToReact(children, options)}</Link>
+            </Strong>
+          );
 
-      if (name === 'a') {
-        return (
-          <Strong>
-            <Link to={attribs.href}>{domToReact(children, options)}</Link>
-          </Strong>
-        );
+        case 'h1':
+          return (
+            <TextWrapper
+              as={H1}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'h2':
+          return (
+            <TextWrapper
+              as={H2}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'h3':
+          return (
+            <TextWrapper
+              as={H3}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'h4':
+          return (
+            <TextWrapper
+              as={H4}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'h5':
+          return (
+            <TextWrapper
+              as={H5}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'h6':
+          return (
+            <TextWrapper
+              as={H6}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'strong':
+          return (
+            <TextWrapper
+              as={Strong}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'p':
+          return (
+            <TextWrapper
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'ul':
+          return (
+            <TextWrapper
+              as="ul"
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+        case 'ol':
+          return (
+            <TextWrapper
+              as="ol"
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              {domToReact(children, options)}
+            </TextWrapper>
+          );
+
+        case 'blockquote':
+          return (
+            <TextWrapper
+              as="blockquote"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                minHeight: '50px',
+              }}
+              alignParagraph={alignParagraph}
+              justifyParagraph={handleJustifyCases(attribs.class)}
+            >
+              <YangQuotes
+                width={20}
+                height={20}
+                style={{ alignSelf: 'flex-start' }}
+              />
+              {/* Arbitrary div to group any html that may split up 'children' */}
+              <div>{domToReact(children, options)}</div>
+              <YinQuotes
+                width={20}
+                height={20}
+                style={{ alignSelf: 'flex-end' }}
+              />
+            </TextWrapper>
+          );
       }
-
-      if (name === 'h1') {
-        return (
-          <TextWrapper
-            as={H1}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-      }
-
-      if (name === 'h2')
-        return (
-          <TextWrapper
-            as={H2}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'h3')
-        return (
-          <TextWrapper
-            as={H3}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'h4')
-        return (
-          <TextWrapper
-            as={H4}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'h5')
-        return (
-          <TextWrapper
-            as={H5}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'h6')
-        return (
-          <TextWrapper
-            as={H6}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'strong')
-        return (
-          <TextWrapper
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-            as={Strong}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'p') {
-        switch (attribs.class) {
-          case 'ql-align-left':
-            return (
-              <TextWrapper
-                alignParagraph={alignParagraph}
-                justifyParagraph="left"
-              >
-                {domToReact(children, options)}
-              </TextWrapper>
-            );
-
-          case 'ql-align-center':
-            return (
-              <TextWrapper
-                alignParagraph={alignParagraph}
-                justifyParagraph="center"
-              >
-                {domToReact(children, options)}
-              </TextWrapper>
-            );
-
-          case 'ql-align-right':
-            return (
-              <TextWrapper
-                alignParagraph={alignParagraph}
-                justifyParagraph="right"
-              >
-                {domToReact(children, options)}
-              </TextWrapper>
-            );
-
-          case 'ql-align-justify': // is there actually a difference between this and left-aligned
-            return (
-              <TextWrapper
-                alignParagraph={alignParagraph}
-                justifyParagraph="left"
-              >
-                {domToReact(children, options)}
-              </TextWrapper>
-            );
-
-          default:
-            break;
-        }
-      }
-
-      if (name === 'ul')
-        return (
-          <TextWrapper
-            as="ul"
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'ol')
-        return (
-          <TextWrapper
-            as="ol"
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            {domToReact(children, options)}
-          </TextWrapper>
-        );
-
-      if (name === 'blockquote')
-        return (
-          <TextWrapper
-            as="blockquote"
-            style={{ display: 'flex', alignItems: 'center', minHeight: '50px' }}
-            alignParagraph={alignParagraph}
-            justifyParagraph={justifyParagraph}
-          >
-            <YangQuotes
-              width={20}
-              height={20}
-              style={{ alignSelf: 'flex-start' }}
-            />
-            {/* Arbitrary div to group any html that may split up 'children' */}
-            <div>{domToReact(children, options)}</div>
-            <YinQuotes
-              width={20}
-              height={20}
-              style={{ alignSelf: 'flex-end' }}
-            />
-          </TextWrapper>
-        );
 
       if (attribs.class === 'ql-divider-standard')
         return <Divider type="standard" />;
