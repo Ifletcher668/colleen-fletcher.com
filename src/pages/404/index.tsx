ccb33234215b1983616e4f4b59676b092ca4db64
Isiah Fletcher <Isiahfletcher7@mail.com>
2022-01-02 11:53:49 -0700

    Uncaught merge conflict

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
 import Layout from '../../components/Layout';
-import { Heading } from '../../components/Text/Heading';
-import { Paragraph } from '../../components/Elements';
 
-export default ({ location }: PageProps): JSX.Element => {
+export default (props: PageProps): JSX.Element => {
   return (
-    <Layout location={location}>
-      <Heading level="one">We're sorry!</Heading>
-
-      <Paragraph>We couldn't find what you were looking for.</Paragraph>
+    <Layout location={props.location}>
+      404
+      {/*  */}
     </Layout>
   );
 };
